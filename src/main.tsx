6c64481f37eaff6c4691b013e20d0dcc656f470c
chenyunshu <chenyunshu2@gmail.com>
2025-03-02 23:50:14 +0800

    fix: fixed bugs

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { MenuProvider } from './providers/MenuProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <MenuProvider>
-      <App />
-    </MenuProvider>
+    <App />
   </StrictMode>,
 )
