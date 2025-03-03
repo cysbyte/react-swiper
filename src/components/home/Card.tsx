a5d72b2ecf66c090e3a35e3b2923d41374e8e6bc
chenyunshu <chenyunshu2@gmail.com>
2025-03-04 02:38:44 +0800

    feat: update swiper

--- src/components/home/Card.tsx
+++ src/components/home/Card.tsx
@@ -6,9 +6,9 @@ const Card = ( {image, price, currency, description} : {image: string, price: st
     <div className='relative h-full w-[225px] flex flex-col justify-center items-center'>
         <img className='absolute top-0 left-0 w-full h-full object-cover' src={image} alt="card" />
         <div className='relative z-10 w-full h-full flex justify-center items-center'>
-            <div className='flex flex-col'>
-                <p className='text-[36px] leading-[40px] font-bold font-openSans text-white'>{price} {currency}</p>
-                <p className='text-[16px] leading-[24px]  font-normal font-openSans text-white'>{description}</p>
+            <div className='flex flex-col gap-2'>
+                <p className='text-[25px] leading-[30px] font-bold font-openSans text-white'>{price} {currency}</p>
+                <p className='text-[14px] leading-[18px]  font-normal font-openSans text-white'>{description}</p>
             </div> 
         </div>
         <div className='relative z-10 rounded-2xl w-[180px] h-[55px] mb-24 md:mb-10 bg-white flex  items-center justify-between px-3'>
